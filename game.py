22aa52e43d8d4d2c3b3934f0d1ac32eb16d7081f
theGitGoblin <jclarke16@ggc.edu>
2025-03-02 11:32:38 -0500

    Input for width/height and white background

--- game.py
+++ game.py
@@ -3,25 +3,31 @@ import pygame
 pygame.init()
 
 
+width = int(input("What size width do you want?(300 - 500): "))
 
+height = int(input("What height width do you want?(300 - 500): "))
 is_initialized = pygame.get_init()
 
 
-myDisplay = pygame.display.set_mode((600, 600), pygame.RESIZABLE)
+myDisplay = pygame.display.set_mode((width, height))
 
+
+pygame.draw.rect(myDisplay, (10, 25, 255 ), [100, 100, 400, 100], 0)
 pygame.display.set_caption('Beans-Tac-Toe')
 
 
 gamerunning = True
 
 while gamerunning:
+    myDisplay.fill((255, 255, 255))
+    pygame.display.flip()
 
 
     for event in pygame.event.get():
 
 
         if event.type == pygame.QUIT:
-            running = False
+            gamerunning = False
 
 
 pygame.quit()
